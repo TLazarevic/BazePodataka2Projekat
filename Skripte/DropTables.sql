9a80d8b773240b2c9f97aadafe8a8495d19c1e99
huehue12 <lazarevict97@gmail.com>
2020-01-26 18:32:39 +0100

    sitne izmene, napisan upit

--- Skripte/DropTables.sql
+++ Skripte/DropTables.sql
@@ -1,1 +1,38 @@
-select 'drop table ', table_name, 'cascade constraints;' from user_tables;
+
+'DROPTABLE' TABLE_NAME                     'CASCADECONSTRAINTS;
+----------- ------------------------------ --------------------
+drop table  ASTRO_OBUCENI_ZA_TM            cascade constraints;
+drop table  ASTRO_UCES_M                   cascade constraints;
+drop table  ASTRONAUT                      cascade constraints;
+drop table  CENTRI_I_POSTROJENJA           cascade constraints;
+drop table  DESTINACIJA                    cascade constraints;
+drop table  DRZAVA                         cascade constraints;
+drop table  GORIVO                         cascade constraints;
+drop table  GRAD                           cascade constraints;
+drop table  JE_OPCIJA                      cascade constraints;
+drop table  LETI_NA                        cascade constraints;
+drop table  M_SA_TM                        cascade constraints;
+
+'DROPTABLE' TABLE_NAME                     'CASCADECONSTRAINTS;
+----------- ------------------------------ --------------------
+drop table  MISIJA                         cascade constraints;
+drop table  ORGANIZUJE                     cascade constraints;
+drop table  PILO_VOZE_TR                   cascade constraints;
+drop table  PILOT                          cascade constraints;
+drop table  PISTA                          cascade constraints;
+drop table  POLECE                         cascade constraints;
+drop table  R_SA_TR_SA_M_SA_TM             cascade constraints;
+drop table  RAKETA                         cascade constraints;
+drop table  STRANA_AGENCIJA                cascade constraints;
+drop table  TIP_MISIJE                     cascade constraints;
+drop table  TIP_RAKETE                     cascade constraints;
+
+'DROPTABLE' TABLE_NAME                     'CASCADECONSTRAINTS;
+----------- ------------------------------ --------------------
+drop table  TIP_RAKETE_SA_TIPOM_MISIJE     cascade constraints;
+drop table  TROSI                          cascade constraints;
+drop table  UPRAVLJA                       cascade constraints;
+drop table  ZAPOSLENI                      cascade constraints;
+
+26 rows selected. 
+
--- Skripte/Upit.sql
+++ Skripte/Upit.sql
@@ -1,6 +1,12 @@
-CREATE VIEW MojiLetovi AS
+CREATE or replace VIEW MojiLetovi AS
 SELECT ALL
-    TRNaz,MisDatPoc as "Naziv, Datum pocetka"
-FROM Upravlja join R_SA_TR_SA_M_SA_TM 
-ON Upravlja.RaId=R_SA_TR_SA_M_SA_TM.RaId 
-join Misija ON Misija.MId=R_SA_TR_SA_M_SA_TM.MId;
+    TRNaz as "TipRakete",DeId as "Destinacija",MDatPoc as "Datum pocetka misije"
+FROM Upravlja 
+join rakete_sa_tr_sa_misijama_sa_tm ON Upravlja.RaId=rakete_sa_tr_sa_misijama_sa_tm.RaId 
+join Misija ON Misija.MId=rakete_sa_tr_sa_misijama_sa_tm.MId
+join raketa on raketa.raid=rakete_sa_tr_sa_misijama_sa_tm.raid
+join tip_rakete on tip_rakete.trid=raketa.trid
+join zaposleni on zaposleni.zapid=upravlja.zapid
+join leti_na on leti_na.mid=upravlja.mid
+WHERE zapime='Tamara' and zapprez='Lazarevic'
+ORDER BY MdatPoc DESC;
